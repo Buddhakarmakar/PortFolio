2b6b4e25ac2593a828e94df79f575b1ddf725e72
Buddhadeb <buddhadebkarmakar4567@gmail.com>
2022-06-28 03:36:45 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { timeInterval } from 'rxjs';
+import { TypeWriter } from './typeWriter';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'my-blog-page';
+  @ViewChild('typeWriter') typeWriter!: ElementRef;
+  el = document.getElementById('typeWriter');
+
+  myData: string[] = [
+    'I am Creative.',
+    'I Love Design.',
+    'I Love to Develop.'];
+  currentData = '';
+
+  ngOnInit() {}
+  ngAfterViewInit() {
+    console.log('hi', this.typeWriter.nativeElement.innerHTML);
+    new TypeWriter(this.typeWriter, this.myData, 2000).tick();
+  }
 }
